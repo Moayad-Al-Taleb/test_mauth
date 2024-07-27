1435e30aa8488685352d4739c0abc3436ec5434f
Moayad Al-Taleb <moayadaltalb@gmail.com>
2024-07-27 22:06:28 +0300

    fix bugs

--- routes/admin.php
+++ routes/admin.php
@@ -31,11 +31,15 @@ Route::prefix('v1')->group(function () {
     Route::prefix('admin')->group(function () {
         Route::middleware(['jwt.verify'])->group(function () {
             ##### Posts Routes #####
-            Route::apiResource('posts', PostController::class);
-            // Route::get('posts/trashed', [PostController::class, 'trashed'])->name('posts.trashed');
-            Route::post('posts/trashed', [PostController::class, 'trashed'])->name('posts.trashed');
-            Route::post('posts/{id}/restore', [PostController::class, 'restore'])->name('posts.restore');
+            Route::get('posts', [PostController::class, 'index'])->name('posts.index');
+            Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+            Route::get('posts/trashed', [PostController::class, 'trashed'])->name('posts.trashed');
             Route::delete('posts/{id}/force-delete', [PostController::class, 'forceDelete'])->name('posts.forceDelete');
+            Route::post('posts/{id}/restore', [PostController::class, 'restore'])->name('posts.restore');
+            Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+            Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
+            Route::patch('posts/{post}', [PostController::class, 'update'])->name('posts.update'); // To cover PATCH method
+            Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
             ##### End Posts Routes #####
         });
     });
